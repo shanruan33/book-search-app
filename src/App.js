1d6146d1725bef603d73a0fb007b83eb4a6f2f1e
Shan <shanruan333@gmail.com>
2020-07-29 19:35:16 -0400

    task 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState('Christmas');
+  const [search, setSearch] = useState("");
+
+  // recieve input from form
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+  // restructure to query format and set query state
+  const getSearch = e => {
+    e.preventDefault();
+    var formatedSearch = search.split(' ').join('%20');
+    // console.log(search, formatedSearch);
+    setQuery(formatedSearch);
+    setSearch("");
+  }
+  // fetch for data (usually use fetch())
+  useEffect(() => {
+    //max set to 15 (max = 0 is not working, default is 10)
+    axios.get(`https://reststop.randomhouse.com/resources/titles?max=15&expandLevel=1&search=${query}`
+    ).then(res => {
+      // console.log(res);
+      res = res.data.title;
+      setBooks(res);
+    }).catch(err => console.log(err));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* search form */}
+      <div className="search">
+        <input className="search-bar" type="text" placeholder="Search book title..." onChange={updateSearch} />
+        <button className="btn search-btn" onClick={getSearch} >Search</button>
+      </div>
+    {/* search results */}
+    </>
+  )
+
 }
 
 export default App;
